c50701be4053ca6877f05f4a28c60eca616967d5
Jatin_Saini <sainijatin247@gmail.com>
2022-10-29 17:17:17 -0400

    replies added

--- models/Pizza.js
+++ models/Pizza.js
@@ -36,9 +36,9 @@ const PizzaSchema = new Schema({
   );
 
   // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.length;
-});
+  PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+  });
 const Pizza = model('Pizza', PizzaSchema);
 
 module.exports = Pizza;
